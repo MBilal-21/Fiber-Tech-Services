663e71776021a5ac17890a0aa18f08d9f880319f
Bilal Ashraf <bilalm3313@gmail.com>
2024-09-09 18:35:21 +0500

    add meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,8 @@ import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToUpBtn";
 import Loading from "@/components/Loading"; // Import Loading component
 import { Suspense } from "react"; // Import Suspense
-
+import Head from "next/head";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,16 +17,35 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="GXW489isAHnbMmnIQk2Zk7PhY4HUMtgpBKffVJGbLKI"
+        />
+      </Head>
       <body className={inter.className}>
         <ScrollToTop />
         <Navbar />
-        
+
         {/* Wrap ClientLayout in Suspense to show a loading screen */}
         <Suspense fallback={<Loading />}>
           <main className="overflow-x-hidden">{children}</main>
         </Suspense>
 
         <Footer />
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7ZQMEKKJ1L"
+          strategy="afterInteractive"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7ZQMEKKJ1L');`}
+        </Script>
       </body>
     </html>
   );
